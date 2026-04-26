8f357f4c17a781d1d073071131bd37487089c724
Arafat <arafatrahman995011@gmail.com>
2026-04-26 14:30:00 +0600

    Stage 2 - Tasks 35-38: JSON serialize/deserialize User and ChatMessage

--- ChatApp.Console/Program.cs
+++ ChatApp.Console/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ChatApp.Console.Models;
-
+using System.Text.Json;
 
 class Program
 {
@@ -17,14 +17,50 @@ class Program
         Server server = new Server("MainServer");
         server.PrintStatus();
 
-        // Create three Message objects with different content
-            Message message1 = new Message("Kakka", "-_-");
-            Message message2 = new Message("Mama", "How's it going?");
-            Message message3 = new Message("Jaaanss", "Ailabiu");
+        // Json Shekhar part. 
+        // ------------------==================-------------------------------
+        // ------------------==================-------------------------------
 
-            // Print the messages in the required format
-            System.Console.WriteLine(message1);
-            System.Console.WriteLine(message2);
-            System.Console.WriteLine(message3);
+        string JsonString1 = JsonSerializer.Serialize(user1);
+        System.Console.WriteLine(JsonString1);
+        
+        // Deserialize 
+        User deserializedJson = JsonSerializer.Deserialize<User>(JsonString1);
+        System.Console.WriteLine(deserializedJson.Username);
+
+        // Task 37: Serialize and Deserialize a ChatMessage object
+        ChatMessage chatMessage = new ChatMessage("Alice", "Hello there", "General");
+
+        // Serialize the ChatMessage to JSON
+        string chatMessageJson = JsonSerializer.Serialize(chatMessage);
+        Console.WriteLine("\nSerialized ChatMessage JSON: ");
+        Console.WriteLine(chatMessageJson);
+
+        // Deserialize the ChatMessage JSON back to an object
+        ChatMessage deserializedChatMessage = JsonSerializer.Deserialize<ChatMessage>(chatMessageJson);
+        Console.WriteLine($"Deserialized ChatMessage - Sender: {deserializedChatMessage.SenderName}, Text: {deserializedChatMessage.Text}, Room: {deserializedChatMessage.RoomName}");
+
+        // Task 38: Serialize and Deserialize a List  ChatMessage objects
+        List<ChatMessage> chatMessages = new List<ChatMessage>
+        {
+            new ChatMessage("Kalu", "Hello Kakka", "General"),
+            new ChatMessage("Dholu", "Hi, Kakka!", "General"),
+            new ChatMessage("Kakka", "Good evening everyone!", "General")
+        };
+
+        // Serialize
+        string chatMessagesJson = JsonSerializer.Serialize(chatMessages, new JsonSerializerOptions { WriteIndented = true });
+        Console.WriteLine("\nSerialized List of ChatMessages JSON: ");
+        Console.WriteLine(chatMessagesJson);
+
+        // Deserialize 
+        List<ChatMessage> deserializedChatMessages = JsonSerializer.Deserialize<List<ChatMessage>>(chatMessagesJson);
+
+        //print each message
+        Console.WriteLine("\nDeserialized List of ChatMessages: ");
+        foreach (var message in deserializedChatMessages)
+        {
+            Console.WriteLine(message.ToString());
+        }
     }
 }
--- ChatApp.Console/obj/Debug/net8.0/ChatApp.Console.AssemblyInfo.cs
+++ ChatApp.Console/obj/Debug/net8.0/ChatApp.Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApp.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ee840f77ea79816459f068c21806078e13db164")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApp.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApp.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
